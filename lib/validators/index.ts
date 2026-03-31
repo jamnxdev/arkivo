ee331f85424e089f456a33b4149ff95b782cc1fb
CJ <chovatiajaimin@gmail.com>
2026-03-31 22:05:39 +0200

    chore: add receipt validator

--- lib/validators/index.ts
+++ lib/validators/index.ts
@@ -1,1 +1,1 @@
-export {};
+export * from "./receipt";
---
+++ lib/validators/receipt.ts
@@ -0,0 +1,32 @@
+import z from "zod/v3";
+
+export const receiptSchema = z.object({
+  merchant: z.string().nullable(),
+  merchant_brand: z.string().nullable(),
+
+  total: z.number().nullable(),
+  currency: z.string().default("EUR"),
+
+  date: z.string().nullable(),
+  time: z.string().nullable(),
+
+  category: z.string().nullable(),
+
+  raw_text: z.string().optional(),
+
+  items: z
+    .array(
+      z.object({
+        name: z.string(),
+        price: z.number(),
+        category: z.string().optional(),
+      }),
+    )
+    .nullable(),
+
+  tax: z.record(z.number()).nullable(),
+
+  metadata: z.record(z.any()).optional(),
+
+  parser_config_id: z.string().optional(),
+});
